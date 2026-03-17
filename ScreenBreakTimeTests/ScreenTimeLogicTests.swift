1fbf20d1bbd5ac223e10fd1238ea7df6ed0b566e
Moto ITO <motonari.ito@gmail.com>
2026-03-17 15:56:00 -0700

    Update tests for the recent renaming.

--- ScreenBreakTimeTests/ScreenTimeLogicTests.swift
+++ ScreenBreakTimeTests/ScreenTimeLogicTests.swift
@@ -1,54 +1,54 @@
 //
 
-import Testing
-import ScreenBreakTime
 import Foundation
+import ScreenBreakTime
+import Testing
 
 struct ScreenTimeLogicTests {
-    
+
     @Test func emptyRecords() async throws {
-        let remainingTime = calculateRemainingTime(
-            records: [],
+        let remainingTime = findRemainingScreenTime(
+            events: [],
             currentTime: .now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 9)
     }
-    
+
     @Test func rightAfterStart() async throws {
         let now = Date.now
-        let record = ScreenTime(timestamp: now, state: .active)
-        let remainingTime = calculateRemainingTime(
-            records: [record],
+        let event = Event(timestamp: now, state: .active)
+        let remainingTime = findRemainingScreenTime(
+            events: [event],
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 9)
     }
-    
+
     @Test func usingMoreThanWindowInterval() async throws {
         let now = Date.now
-        let record = ScreenTime(timestamp: now.addingTimeInterval(-11), state: .active)
-        let remainingTime = calculateRemainingTime(
-            records: [record],
+        let event = Event(timestamp: now.addingTimeInterval(-11), state: .active)
+        let remainingTime = findRemainingScreenTime(
+            events: [event],
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
-        #expect(remainingTime == 0)
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
+        #expect(remainingTime == 9)
     }
 
     @Test func timeUp() async throws {
         let now = Date.now
-        let record = ScreenTime(timestamp: now.addingTimeInterval(-9), state: .active)
-        let remainingTime = calculateRemainingTime(
-            records: [record],
+        let event = Event(timestamp: now.addingTimeInterval(-9), state: .active)
+        let remainingTime = findRemainingScreenTime(
+            events: [event],
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 0)
     }
 
@@ -56,17 +56,17 @@ struct ScreenTimeLogicTests {
         // ***----***
         //
         let now = Date.now
-        let records = [
-            ScreenTime(timestamp: now.addingTimeInterval(-3), state: .active),
-            ScreenTime(timestamp: now.addingTimeInterval(-7), state: .inactive),
-            ScreenTime(timestamp: now.addingTimeInterval(-10), state: .active),
+        let events = [
+            Event(timestamp: now.addingTimeInterval(-3), state: .active),
+            Event(timestamp: now.addingTimeInterval(-7), state: .inactive),
+            Event(timestamp: now.addingTimeInterval(-10), state: .active),
         ]
-        let remainingTime = calculateRemainingTime(
-            records: records,
+        let remainingTime = findRemainingScreenTime(
+            events: events,
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 6)
     }
 
@@ -75,66 +75,66 @@ struct ScreenTimeLogicTests {
         // 1 more : -*********, OK
         // 2 more : **********, NG
         let now = Date.now
-        let records = [
-            ScreenTime(timestamp: now.addingTimeInterval(-8), state: .active),
-            ScreenTime(timestamp: now.addingTimeInterval(-9), state: .inactive),
-            ScreenTime(timestamp: now.addingTimeInterval(-10), state: .active),
+        let events = [
+            Event(timestamp: now.addingTimeInterval(-8), state: .active),
+            Event(timestamp: now.addingTimeInterval(-9), state: .inactive),
+            Event(timestamp: now.addingTimeInterval(-10), state: .active),
         ]
-        let remainingTime = calculateRemainingTime(
-            records: records,
+        let remainingTime = findRemainingScreenTime(
+            events: events,
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 1)
     }
 
     @Test func afterResting() async throws {
         let now = Date.now
-        let records = [
-            ScreenTime(timestamp: now.addingTimeInterval(-5), state: .inactive),
-            ScreenTime(timestamp: now.addingTimeInterval(-10), state: .active),
+        let events = [
+            Event(timestamp: now.addingTimeInterval(-5), state: .inactive),
+            Event(timestamp: now.addingTimeInterval(-10), state: .active),
         ]
-        let remainingTime = calculateRemainingTime(
-            records: records,
+        let remainingTime = findRemainingScreenTime(
+            events: events,
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 5)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 5)
+
         #expect(remainingTime == 5)
     }
 
     @Test func forciblyTerminatedCorruptedRecord() async throws {
         let now = Date.now
-        let records = [
-            ScreenTime(timestamp: now.addingTimeInterval(-70), state: .active),
+        let events = [
+            Event(timestamp: now.addingTimeInterval(-70), state: .active)
         ]
-        let remainingTime = calculateRemainingTime(
-            records: records,
+        let remainingTime = findRemainingScreenTime(
+            events: events,
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 9)
     }
 
     @Test func hasBeenActiveSinceLongTimeAgo() async throws {
         let now = Date.now
-        let records = [
-            ScreenTime(timestamp: now.addingTimeInterval(-10), state: .heartBeat),
-            ScreenTime(timestamp: now.addingTimeInterval(-20), state: .heartBeat),
-            ScreenTime(timestamp: now.addingTimeInterval(-30), state: .heartBeat),
-            ScreenTime(timestamp: now.addingTimeInterval(-40), state: .heartBeat),
-            ScreenTime(timestamp: now.addingTimeInterval(-50), state: .heartBeat),
-            ScreenTime(timestamp: now.addingTimeInterval(-60), state: .heartBeat),
-            ScreenTime(timestamp: now.addingTimeInterval(-70), state: .active),
+        let events = [
+            Event(timestamp: now.addingTimeInterval(-10), state: .heartBeat),
+            Event(timestamp: now.addingTimeInterval(-20), state: .heartBeat),
+            Event(timestamp: now.addingTimeInterval(-30), state: .heartBeat),
+            Event(timestamp: now.addingTimeInterval(-40), state: .heartBeat),
+            Event(timestamp: now.addingTimeInterval(-50), state: .heartBeat),
+            Event(timestamp: now.addingTimeInterval(-60), state: .heartBeat),
+            Event(timestamp: now.addingTimeInterval(-70), state: .active),
         ]
-        let remainingTime = calculateRemainingTime(
-            records: records,
+        let remainingTime = findRemainingScreenTime(
+            events: events,
             currentTime: now,
-            movingWindowInterval: 10,
-            maxActiveTimeInterval: 9)
-        
+            lookBackDuration: 10,
+            maxScreenTime: 9)
+
         #expect(remainingTime == 0)
     }
 }
